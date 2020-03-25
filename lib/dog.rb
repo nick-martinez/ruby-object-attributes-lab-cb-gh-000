0224279d1b0839f38f2dbac581d35535425ce941
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-25 17:45:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,22 @@
 class Dog
-  def name= (name) #Getter for name
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
 
   def name #Setter for name
     @name
   end
 
-  def breed= (breed)
-    @breed = breed
+  def name= (new_name) #Getter for name
+    @name = new_name
   end
 
   def breed
     @breed
+  end
+
+  def breed= (new_breed)
+    @breed = new_breed
   end
 end
